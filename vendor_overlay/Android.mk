7369e2e0f7e5844dc590d5cb19083f7fea879fba
Robert Balmbra <robbalmbra@gmail.com>
2020-09-03 13:34:14 +0100

    Update Android.mk

--- vendor_overlay/Android.mk
+++ vendor_overlay/Android.mk
@@ -33,14 +33,6 @@ LOCAL_MODULE_PATH       := $(TARGET_OUT_PRODUCT)/vendor_overlay/$(PRODUCT_TARGET
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE            := audio_policy_configuration.xml
-LOCAL_MODULE_TAGS       := optional
-LOCAL_MODULE_CLASS      := ETC
-LOCAL_SRC_FILES         := etc/audio_policy_configuration.xml
-LOCAL_MODULE_PATH       := $(TARGET_OUT_PRODUCT)/vendor_overlay/$(PRODUCT_TARGET_VNDK_VERSION)/etc
-include $(BUILD_PREBUILT)
-
-include $(CLEAR_VARS)
 LOCAL_MODULE            := media_profiles_V1_0.xml
 LOCAL_MODULE_TAGS       := optional
 LOCAL_MODULE_CLASS      := ETC
